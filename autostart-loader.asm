772c242d914a975a479d3c2ebab8af8d692fd713
William Astle <lost@l-w.ca>
2022-01-31 22:05:14 -0600

    Update comments for the autostart.

--- autostart-loader.asm
+++ autostart-loader.asm
@@ -5,6 +5,10 @@
 ; also to LOADER, it means that even if, for some reason, control doesn't transfer here, EXEC will still do so.
 LOADER		clr $FF40			; turn off drive motors
 		jmp ONCE			; transfer control to the main stream code
+; This is the bit that intercepts RVEC12. It requires at least Extended Basic to work because it relies on multi-origin binaries.
+; That will always be the case for Disk Basic. Theoretically, with just this, a loader will work from tape in Extended Basic
+; or higher, too.
 		org $182			; address of RVEC12
 		jmp LOADER			; transfer control to our loader
+; In case RVEC12 doesn't trigger for some reason, this allows EXEC to also start the loader.
 		end LOADER
