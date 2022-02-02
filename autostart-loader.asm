afc25762719b5704c54596881a78e8404a3b7791
William Astle <lost@l-w.ca>
2022-02-01 20:55:26 -0600

    Make IRQ handler play nice with Basic.
    Make IRQ handler actually call the Disk Basic handler (or Extended Basic
    handler if no Disk Basic). This assumes the IRQ handler is in the right
    place for the detected version of Disk Basic. The main reason for this is so
    the the disk motor timeout thing actually works.

--- autostart-loader.asm
+++ autostart-loader.asm
@@ -8,21 +8,24 @@ LOADER		clr $FF40			; turn off drive motors
 		sta $191			; set up handler for the error routine at AC46
 		stx $192
 		ldx $C004			; get DSKCON address pointer
+		ldy #$8955			; point to IRQ handler
 		cmpx #$D75F			; is it disk basic 1.1?
 		beq load_disk11			; brif so
 		cmpx #$d66c			; is it disk basic 1.0?
 		beq load_disk10			; brif so
 		ldx #disk_error			; point to the routine for no disk system
-		stx hook_openi			; set the disk basic vectors to go there
-		stx hook_openo
+		ldu #disk_error
 		bra load_001			; go finish initializing
 load_disk10	ldx #$C956			; address of "open system file for output" routine
 		ldu #$C959			; address of "open system file for input" routine
+		ldy #$d7c4			; point to Disk IRQ handler
 		bra load_000			; go set vectors
 load_disk11	ldx #$CA04			; address of "open system file for output" routine
 		ldu #$CA07			; address of "Open system file for input" routine
+		ldy #$d8b7			; point to Disk IRQ handler
 load_000	stx hook_openo			; save address of "open for output"
 		stu hook_openi			; save address of "open for input"
+		sty hook_irq			; save IRQ hook for Basic
 load_001	clr error_handler		; mark the error handler as disabled
 		clr error_handler+1
 		jmp ONCE			; transfer control to the main stream code
