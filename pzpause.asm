ad9003cda924812f1e712b7f5e5ac8ceca018236
William Astle <lost@l-w.ca>
2022-02-13 21:03:23 -0600

    ZPAUSE now has a way to resume the game
    ZPAUSE now takes the game into pause mode. Pause mode allows command entry
    but does not allow any game play commands. The "RESUME" command restarts the
    game.

--- pzpause.asm
+++ pzpause.asm
@@ -1,6 +1,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; This is the "pause mode". Pause mode has a completely different list of commands available to it.
-PZPAUS	        inc ZFLAG       		; freeze the game
+PZPAUS	        dec SLEEP       		; freeze the scheduler
+                clr HBEATF                      ; stop the heart
 		SWI                             ;clear the status line
                 FCB     CLRSTS          	; clear the status bar
 		ldu #TXTSTS			; point to status area parameters
@@ -10,4 +11,29 @@ PZPAUS	        inc ZFLAG       		; freeze the game
 		jsr putstrimm			; display the pause notice
 		fcn '**PAUSED**'
 		clr TXBFLG			; reset display parameters
-PZPA00  	bra *
+                dec PAUSED
+                rts
+; This is the puase mode command handler
+pausemodecmd	ldx #kwlist_pcmd		; point to command list
+		jsr PARSER			; look up word in command list
+		beq pausemode000		; brif nothing to match
+		bpl pausemode001		; brif found
+		jsr CMDERR			; show bad command string
+pausemode000	jmp HMAN70			; go on with new command
+pausemode001	lsla				; two bytes per jump table entry
+		ldx #pausecmd_jump		; point to command jump table
+		jsr [a,x]			; go handle command
+		jmp HMAN70			; go handle new command
+; Pause mode command list
+kwlist_pcmd	fcb 1
+		fcb 0				; RESUME
+		fcn 'RESUME'
+; Pause mode command jump table
+pausecmd_jump	fdb pcmd_resume			; RESUME command
+; This is the resume command
+pcmd_resume	SWI                             ;update the screen
+                FCB     STATUS                  ; update the status line
+		dec HBEATF			; enable heartbeat
+		clr SLEEP			; re-enable scheduler
+		clr PAUSED			; turn off pause mode
+		rts
